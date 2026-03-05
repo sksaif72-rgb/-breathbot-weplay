9be5e29916dfdf019df825f4b2625779deb1d54f
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:51:50 +0100

    Update time_utils.py

--- ‏‏utils/time_utils.py
+++ ‏‏utils/time_utils.py
@@ -1,1 +1,23 @@
+from bot.database import pool
 
+
+async def best_minute(result):
+
+    async with pool.acquire() as conn:
+
+        row = await conn.fetchrow(
+            """
+            SELECT minute, COUNT(*) as c
+            FROM training_data
+            WHERE right_result=$1
+            GROUP BY minute
+            ORDER BY c DESC
+            LIMIT 1
+            """,
+            result
+        )
+
+    if not row:
+        return None
+
+    return row["minute"]
