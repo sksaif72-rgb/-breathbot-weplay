08aa3c1c56b242c05d0615c67c4aa21acf49a1e1
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:49:08 +0100

    Update cards.py

--- ‏‏keyboards/cards.py
+++ ‏‏keyboards/cards.py
@@ -1,1 +1,46 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+cards = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
+
+types = ["سِنك","دينار","قلب","ماجه"]
+
+
+def cards_keyboard():
+
+    keyboard = []
+
+    row = []
+
+    for i, card in enumerate(cards):
+
+        row.append(
+            InlineKeyboardButton(
+                text=card,
+                callback_data=f"card_{card}"
+            )
+        )
+
+        if len(row) == 4:
+            keyboard.append(row)
+            row = []
+
+    if row:
+        keyboard.append(row)
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+def type_keyboard():
+
+    keyboard = []
+
+    for t in types:
+
+        keyboard.append([
+            InlineKeyboardButton(
+                text=t,
+                callback_data=f"type_{t}"
+            )
+        ])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
