61e1c0185d678703bca0961b6c146f8c3584d4df
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:53:40 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,27 +6,41 @@ from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.database import connect_db, create_tables
 
-from handlers import start, guess, training, stats, admin
+# handlers
+from handlers import start
+from handlers import guess
+from handlers import training
+from handlers import stats
+from handlers import admin
 
 
 async def main():
 
+    # logs
     logging.basicConfig(level=logging.INFO)
 
-    bot = Bot(BOT_TOKEN)
+    # bot
+    bot = Bot(token=BOT_TOKEN)
+
+    # dispatcher
     dp = Dispatcher()
 
+    # connect database
+    await connect_db()
+
+    # create tables if not exist
+    await create_tables()
+
+    # register routers
     dp.include_router(start.router)
     dp.include_router(guess.router)
     dp.include_router(training.router)
     dp.include_router(stats.router)
     dp.include_router(admin.router)
 
-    await connect_db()
-    await create_tables()
+    print("BREATHBOT-Weplay is running...")
 
-    print("BREATHBOT-Weplay running")
-
+    # start bot
     await dp.start_polling(bot)
 
 
