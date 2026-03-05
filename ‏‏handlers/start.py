119db4cef006a2104a5284e4c113fc3f9c1bc7db
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:45:21 +0100

    Update start.py

--- ‏‏handlers/start.py
+++ ‏‏handlers/start.py
@@ -1,12 +1,32 @@
 from aiogram import Router
+from aiogram.filters import Command
 from aiogram.types import Message
-from keyboards.menus import menu
+
+from bot.database import pool
+from keyboards.menus import main_menu
 
 router = Router()
 
-@router.message(commands=["start"])
-async def start(msg: Message):
 
-    await msg.answer(
-        "اهلاً بك في BREATHBOT-Weplay\n\nاكتب كود الاشتراك"
+@router.message(Command("start"))
+async def start(message: Message):
+
+    telegram_id = message.from_user.id
+    username = message.from_user.username
+
+    async with pool.acquire() as conn:
+
+        await conn.execute(
+            """
+            INSERT INTO users(telegram_id, username)
+            VALUES($1,$2)
+            ON CONFLICT(telegram_id) DO NOTHING
+            """,
+            telegram_id,
+            username
+        )
+
+    await message.answer(
+        "مرحبا بك في BREATHBOT-Weplay\n\n"
+        "اكتب كود الاشتراك"
     )
