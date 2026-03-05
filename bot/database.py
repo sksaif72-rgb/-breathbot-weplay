d546180083baa1d0e5d73efbe53c5dbc43265b49
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:21:14 +0100

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -1,1 +1,11 @@
+import asyncpg
+from bot.config import DATABASE_URL
 
+pool = None
+
+async def connect():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+async def get_pool():
+    return pool
