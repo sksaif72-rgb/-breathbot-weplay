1723c4ed17105fe29f3c6a258fd96764d7b42ab2
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:22:48 +0100

    Update start.py

--- ‏‏handlers/start.py
+++ ‏‏handlers/start.py
@@ -1,1 +1,12 @@
+from aiogram import Router
+from aiogram.types import Message
+from keyboards.menus import menu
 
+router = Router()
+
+@router.message(commands=["start"])
+async def start(msg: Message):
+
+    await msg.answer(
+        "اهلاً بك في BREATHBOT-Weplay\n\nاكتب كود الاشتراك"
+    )
