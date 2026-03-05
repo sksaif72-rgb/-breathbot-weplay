a25125eaaba08daba41f1b321f99b881e6e6a87b
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 00:21:43 +0100

    Update menus.py

--- ‏‏keyboards/menus.py
+++ ‏‏keyboards/menus.py
@@ -1,1 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+menu = ReplyKeyboardMarkup(
+keyboard=[
+[KeyboardButton(text="بدء التخمين")],
+[KeyboardButton(text="الاحصائيات")],
+[KeyboardButton(text="حسابي")],
+[KeyboardButton(text="تواصل مع الدعم")]
+],
+resize_keyboard=True
+)
