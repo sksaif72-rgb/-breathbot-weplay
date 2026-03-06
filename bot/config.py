3b7cb3671e1deba5dd283d08587e710dba945295
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 03:43:29 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,10 @@
 import os
 
+# توكن البوت
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# كود وضع التدريب
+TRAINING_CODE = os.getenv("TRAINING_CODE", "12345")
+
+# رابط قاعدة البيانات Neon
 DATABASE_URL = os.getenv("DATABASE_URL")
