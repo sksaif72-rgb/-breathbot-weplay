0239294eb5a8ab31294b3c7ba55938e70f261ba8
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 01:14:05 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,4 +3,6 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+TRAINING_CODE = os.getenv("TRAINING_CODE")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
