55514eed2d5c89d42ecc4686708eb496810a0e7f
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 01:05:15 +0100

    Update training.py

--- ‏‏keyboards/training.py
+++ ‏‏keyboards/training.py
@@ -1,19 +1,71 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+cards = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
 
-def right_results_keyboard():
+types = ["سِنك","دينار","قلب","ماجه"]
 
-    results = [
-        "زوجين",
-        "متتالية",
-        "ثلاثة",
-        "فل هاوس",
-        "اربعة"
-    ]
+right_results = [
+"زوجين",
+"متتالية",
+"ثلاثة",
+"فل هاوس",
+"اربعة"
+]
+
+left_results = [
+"زوج",
+"متتالية نفس النوع",
+"AA",
+"زوج و متتالية",
+"لاشيء"
+]
+
+
+def training_cards_keyboard():
+
+    keyboard = []
+    row = []
+
+    for card in cards:
+
+        row.append(
+            InlineKeyboardButton(
+                text=card,
+                callback_data=f"tcard_{card}"
+            )
+        )
+
+        if len(row) == 4:
+            keyboard.append(row)
+            row = []
+
+    if row:
+        keyboard.append(row)
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+def training_types_keyboard():
 
     keyboard = []
 
-    for r in results:
+    for t in types:
+
+        keyboard.append([
+            InlineKeyboardButton(
+                text=t,
+                callback_data=f"ttype_{t}"
+            )
+        ])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+def right_results_keyboard():
+
+    keyboard = []
+
+    for r in right_results:
 
         keyboard.append([
             InlineKeyboardButton(
@@ -27,17 +79,9 @@ def right_results_keyboard():
 
 def left_results_keyboard():
 
-    results = [
-        "زوج",
-        "متتالية نفس النوع",
-        "AA",
-        "زوج و متتالية",
-        "لاشيء"
-    ]
-
     keyboard = []
 
-    for r in results:
+    for r in left_results:
 
         keyboard.append([
             InlineKeyboardButton(
