fa29b90939b2d76552924c25b42efdb1c50c78d7
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 01:11:53 +0100

    Update menus.py

--- ‏‏keyboards/menus.py
+++ ‏‏keyboards/menus.py
@@ -1,24 +1,29 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def main_menu():
 
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
 
-            [
-                InlineKeyboardButton(
-                    text="بدء التخمين",
-                    callback_data="start_guess"
-                )
-            ],
-
-            [
-                InlineKeyboardButton(
-                    text="الاحصائيات",
-                    callback_data="stats"
-                )
-            ]
-
-        ]
+    kb.add(
+        KeyboardButton("🎯 تخمين"),
+        KeyboardButton("📊 احصائيات")
     )
+
+    return kb
+
+
+def training_menu():
+
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    kb.add(
+        KeyboardButton("AA"),
+        KeyboardButton("اربعة"),
+        KeyboardButton("فل هاوس"),
+        KeyboardButton("زوج"),
+        KeyboardButton("متتالية"),
+        KeyboardButton("لا شيء")
+    )
+
+    return kb
