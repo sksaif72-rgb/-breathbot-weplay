1beafbd7906e9c4150a730dc31400423c58ce3b5
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 02:32:23 +0100

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -3,10 +3,34 @@ import os
 
 pool = None
 
-async def connect_db():
 
+async def connect_db():
     global pool
 
     pool = await asyncpg.create_pool(
         os.getenv("DATABASE_URL")
     )
+
+
+async def add_user(user_id):
+
+    async with pool.acquire() as conn:
+
+        await conn.execute(
+            """
+            INSERT INTO users (user_id)
+            VALUES ($1)
+            ON CONFLICT (user_id) DO NOTHING
+            """,
+            user_id
+        )
+
+
+async def get_user(user_id):
+
+    async with pool.acquire() as conn:
+
+        return await conn.fetchrow(
+            "SELECT * FROM users WHERE user_id=$1",
+            user_id
+        )
