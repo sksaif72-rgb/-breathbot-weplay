81e492d0e36bf22d4371a9a5ba2150bd4ad11bae
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 01:00:45 +0100

    Update training.py

--- ‏‏keyboards/training.py
+++ ‏‏keyboards/training.py
@@ -1,1 +1,49 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+def right_results_keyboard():
+
+    results = [
+        "زوجين",
+        "متتالية",
+        "ثلاثة",
+        "فل هاوس",
+        "اربعة"
+    ]
+
+    keyboard = []
+
+    for r in results:
+
+        keyboard.append([
+            InlineKeyboardButton(
+                text=r,
+                callback_data=f"right_{r}"
+            )
+        ])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+def left_results_keyboard():
+
+    results = [
+        "زوج",
+        "متتالية نفس النوع",
+        "AA",
+        "زوج و متتالية",
+        "لاشيء"
+    ]
+
+    keyboard = []
+
+    for r in results:
+
+        keyboard.append([
+            InlineKeyboardButton(
+                text=r,
+                callback_data=f"left_{r}"
+            )
+        ])
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
