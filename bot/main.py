af20d8ba5d0560b79c27b7288d72b7cc9a576421
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 01:00:01 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,43 +6,45 @@ from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.database import connect_db, create_tables
 
-# handlers
 from handlers import start
 from handlers import guess
 from handlers import training
 from handlers import stats
 from handlers import admin
 
+from bot.scheduler import start_scheduler
+
 
 async def main():
 
-    # logs
     logging.basicConfig(level=logging.INFO)
 
-    # bot
     bot = Bot(token=BOT_TOKEN)
 
-    # dispatcher
     dp = Dispatcher()
 
-    # connect database
-    await connect_db()
-
-    # create tables if not exist
-    await create_tables()
-
-    # register routers
+    # تسجيل الهاندلرز
     dp.include_router(start.router)
     dp.include_router(guess.router)
     dp.include_router(training.router)
     dp.include_router(stats.router)
     dp.include_router(admin.router)
 
-    print("BREATHBOT-Weplay is running...")
+    # الاتصال بقاعدة البيانات
+    await connect_db()
 
-    # start bot
+    # إنشاء الجداول اذا لم تكن موجودة
+    await create_tables()
+
+    print("BREATHBOT-Weplay started")
+
+    # تشغيل Scheduler
+    start_scheduler(bot)
+
+    # تشغيل البوت
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+
     asyncio.run(main())
